6027002c0541771b316b2362e28003a02265d1aa
biezhi <biezhi.me@gmail.com>
2015-03-19 18:22:39 +0800

    -

--- src/main/java/org/unique/commons/utils/FileUtil.java
+++ src/main/java/org/unique/commons/utils/FileUtil.java
@@ -491,7 +491,7 @@ public class FileUtil {
 	 * 获取文件后缀
 	 */
 	public static String getSuffix(String fileName) {
-		return fileName.substring(fileName.lastIndexOf(".") + 1);
+		return fileName.substring(fileName.lastIndexOf("."));
 	}
 
 	/**
